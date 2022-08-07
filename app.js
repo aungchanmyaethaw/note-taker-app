998d12f64f19a3a19f9148dd47a652b8d7e544da
aungchanmyaethaw <aungchanmyaethaw0610@gmail.com>
2022-08-08 00:57:56 +0630

    beta

--- app.js
+++ app.js
@@ -5,45 +5,89 @@ const noteContainerEl = document.querySelector('.note-container');
 const titleInputEl = document.querySelector('#title');
 const toggleBtnEl = document.querySelector('.toggle__btn');
 const root = document.querySelector(':root');
-let theme = 'light';
-window.addEventListener('DOMContentLoaded', () => {
-    setNotestoUI();
-    if (localStorage.getItem('keep.theme')) {
-        root.setAttribute('color-scheme', localStorage.getItem('keep.theme'));
-        theme = localStorage.getItem('keep.theme');
-    }
-    toggleBtnEl.textContent = theme;
-});
-
-toggleBtnEl.addEventListener('click', () => {
-    toggleTheme();
-    const audio = document.querySelector('audio');
-
-    audio.play();
-});
-
+const catagoriesInput = document.querySelector('#categories');
+const sideBarLists = document.querySelector('.categories-container');
+let notes = [];
+let filteredNotes = [];
+let categories = [
+    {
+        id: 1,
+        name: 'hobby',
+    },
+    {
+        id: 2,
+        name: 'coding',
+    },
+    {
+        id: 3,
+        name: 'ideas',
+    },
+    {
+        id: 4,
+        name: 'music lyrics',
+    },
+];
 class Note {
-    constructor(title, body) {
+    constructor(title, body, category) {
         this.title = title;
         this.body = body;
+        this.category = category;
         this.id = Math.floor(Math.random() * 2000);
     }
 }
 
 // --- Toggle Light and Dark Mode
-
+let theme = 'light';
 function toggleTheme() {
     theme === 'light' ? (theme = 'dark') : (theme = 'light');
-    toggleBtnEl.textContent = theme;
+    toggleBtnEl.innerHTML =
+        theme === 'light'
+            ? `<i class="fa-solid fa-lightbulb"></i>`
+            : `<i class="fa-solid fa-cloud-moon"></i>`;
     localStorage.setItem('keep.theme', theme);
     root.setAttribute('color-scheme', theme);
 }
 
+// --- Adding Categories to Form
+
+function addCategories() {
+    categories.forEach((category) => {
+        createCategory(category);
+        createsideBarCategory(category);
+    });
+}
+
+function createCategory(category) {
+    const optionEl = document.createElement('option');
+    optionEl.setAttribute('value', category.id);
+    optionEl.textContent = category.name;
+    catagoriesInput.appendChild(optionEl);
+}
+
+// --- Adding Categories to Sidebar
+
+function createsideBarCategory(category) {
+    const liEl = document.createElement('li');
+    liEl.innerHTML = `<span hidden class="hidden-id">${category.id}</span><span>${category.name}</span>`;
+    sideBarLists.appendChild(liEl);
+}
+
+// --- Filter Notes
+
+function filterNotesByCategory(id) {
+    // let filteredNotes = console.log(notes);
+    filteredNotes = notes.filter((note) => Number(note.category) === id);
+    noteContainerEl.innerHTML = '';
+    filteredNotes.forEach((note, index) => addNewNote(note, index));
+}
+
 // --- Add a new Note
 
-function addNewNote(note) {
+function addNewNote(note, index = 1) {
     const newUInote = document.createElement('div');
     newUInote.classList.add('note');
+    newUInote.setAttribute('data-aos', 'fade-up');
+    newUInote.setAttribute('data-aos-delay', `${index * 300}`);
     newUInote.innerHTML = `
                 <span hidden>${note.id}</span>
                 <h2 class="note__title">${note.title}</h2>
@@ -65,10 +109,7 @@ function addNewNote(note) {
 // --- Get Notes from LocalStorage
 
 function getNotes() {
-    let notes;
-    if (localStorage.getItem('keep.notes') === null) {
-        notes = [];
-    } else {
+    if (localStorage.getItem('keep.notes')) {
         notes = JSON.parse(localStorage.getItem('keep.notes'));
     }
 
@@ -87,7 +128,7 @@ function setNotetoLocalStorage(note) {
 
 function setNotestoUI() {
     const notes = getNotes();
-    notes.forEach((note) => addNewNote(note));
+    notes.forEach((note, index) => addNewNote(note, index));
 }
 
 // --- ShowAlert
@@ -126,6 +167,46 @@ function deleteNote(id) {
     // notes = notes.filter((note) => note.id !== id);
 }
 
+// --- Filtering Notes from SearchBar
+
+window.addEventListener('DOMContentLoaded', () => {
+    setNotestoUI();
+    addCategories();
+    if (localStorage.getItem('keep.theme')) {
+        root.setAttribute('color-scheme', localStorage.getItem('keep.theme'));
+        theme = localStorage.getItem('keep.theme');
+    }
+    toggleBtnEl.innerHTML =
+        theme === 'light'
+            ? `<i class="fa-solid fa-lightbulb"></i>`
+            : `<i class="fa-solid fa-cloud-moon"></i>`;
+});
+
+function filterNotes(keyword) {
+    filteredNotes = notes.filter((note) =>
+        [note.title, note.body].join('').toLowerCase().includes(keyword)
+    );
+    noteContainerEl.innerHTML = '';
+    filteredNotes.forEach((note, index) => addNewNote(note, index));
+}
+
+document.querySelector('#search').addEventListener('keyup', (e) => {
+    const keyword = e.target.value.toLowerCase();
+    filterNotes(keyword);
+});
+
+sideBarLists.addEventListener('click', (e) => {
+    const id = e.target.querySelector('.hidden-id').textContent;
+    filterNotesByCategory(Number(id));
+});
+
+toggleBtnEl.addEventListener('click', () => {
+    toggleTheme();
+    const audio = document.querySelector('audio');
+
+    audio.play();
+});
+
 noteContainerEl.addEventListener('click', (event) => {
     if (event.target.classList.contains('note__view')) {
         const currentNote = event.target.closest('.note');
@@ -146,7 +227,12 @@ formEl.addEventListener('submit', (e) => {
     e.preventDefault();
     const bodyInputEl = document.querySelector('#note');
     if (titleInputEl.value.length > 0 && bodyInputEl.value.length > 0) {
-        const newNote = new Note(titleInputEl.value, bodyInputEl.value);
+        const newNote = new Note(
+            titleInputEl.value,
+            bodyInputEl.value,
+            catagoriesInput.value
+        );
+        console.log(newNote);
         addNewNote(newNote);
         setNotetoLocalStorage(newNote);
         showAlert('New Note Added...!', 'success');
